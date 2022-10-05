7cac426d8a67c00995a1a16fd388a6cf26d3e949
Yaser Odat <yaserodat@outlook.com>
2022-10-05 10:01:24 +0000

    added PostContext.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthContextProvider from './Context/AuthContext'
+import PostContextProvider from './Context/PostContext'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider> 
-
+      <PostContextProvider>
     <App />
+    </PostContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
